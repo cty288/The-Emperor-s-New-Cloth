4c499fdc10ec1fa690dc41a7e4329648695a611d
cty288 <cty288@163.com>
2021-05-12 02:00:27 +0800

    051104

--- Assets/Scripts/BGMPlayerControl.cs
+++ Assets/Scripts/BGMPlayerControl.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGMPlayerControl : MonoBehaviour
-{
+public class BGMPlayerControl : MonoBehaviour {
+    public AudioClip[] bgms;
+
+    private AudioSource audio;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!audio.isPlaying) {
+            AudioClip clip = bgms[Random.Range(0, bgms.Length)];
+            audio.clip = clip;
+            audio.Play();
+        }
     }
 }
